47ede38cffb47c8d037dd987275469f6da16829a
Nic0la <nicolaaprile123@gmail.com>
2025-02-20 13:00:03 +0100

    Add JSON properties for ingredient ID and name
    Added `getIngredientId` and `getIngredientName` methods annotated with `@JsonProperty` to expose ingredient details in JSON responses. This enhances API usability by providing clear and structured ingredient information.

--- src/main/java/com/exercise/giallozafferano_def/model/entity/RecipeIngredient.java
+++ src/main/java/com/exercise/giallozafferano_def/model/entity/RecipeIngredient.java
@@ -1,6 +1,7 @@
 package com.exercise.giallozafferano_def.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -24,4 +25,15 @@ public class RecipeIngredient {
     @MapsId("ingredientId")
     @JoinColumn(name = "ingredient_id")
     Ingredient ingredient;
+
+    @JsonProperty("ingredientId")
+    public Long getIngredientId() {
+        return ingredient != null ? ingredient.getId() : null;
+    }
+
+    @JsonProperty("ingredientName")
+    public String getIngredientName() {
+        return ingredient != null ? ingredient.getName() : null;
+    }
+
 }
